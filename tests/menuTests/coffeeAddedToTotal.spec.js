9b96792a0d814eb8adc97bd0b9086dd8cfac4b59
VoloGor <goralvv@gmail.com>
2026-04-07 13:32:56 +0200

    Remove individual tests and redundand methods

--- tests/menuTests/coffeeAddedToTotal.spec.js
+++ tests/menuTests/coffeeAddedToTotal.spec.js
@@ -1,28 +1,21 @@
 import { test } from '../_fixtures/fixtures';
 import { totalPriceFormatStr } from '../../src/common/priceFormatters';
-import { COFFEE_PRICES } from '../../src/constants';
+import { COFFEE_PRICES, COFFEE_NAMES } from '../../src/constants';
 
-const testParameters = [
-  {
-    title: 'Cappuccino',
-    price: COFFEE_PRICES.cappuccino,
-    click: (menuPage) => menuPage.clickCoffeeCup('Cappuccino'),
-  },
-  {
-    title: 'Espresso',
-    price: COFFEE_PRICES.espresso,
-    click: (menuPage) => menuPage.clickCoffeeCup('Espresso'),
-  },
-];
+let testParameters = [];
 
-testParameters.forEach(({ price, title, click }) => {
-  test(`Check ${title} cost is added to Total on menu page`, async ({
+for (const [key, value] of Object.entries(COFFEE_NAMES)) {
+  testParameters.push({ coffee: value, price: COFFEE_PRICES[key] });
+}
+
+testParameters.forEach(({ price, coffee }) => {
+  test(`Check ${coffee} cost is added to Total on menu page`, async ({
     menuPage,
   }) => {
   const totalPriceStr = totalPriceFormatStr(price);
 
   await menuPage.open();
-  await click(menuPage);
+  await menuPage.clickCoffeeCup(coffee);
 
   await menuPage.assertTotalCheckoutContainsValue(totalPriceStr);
   });
--- tests/menuTests/coffeeHasCorrectCost.spec.js
+++ tests/menuTests/coffeeHasCorrectCost.spec.js
@@ -1,27 +1,20 @@
 import { test } from '../_fixtures/fixtures';
 import { priceFormatStr } from '../../src/common/priceFormatters';
-import { COFFEE_PRICES } from '../../src/constants';
+import { COFFEE_NAMES, COFFEE_PRICES } from '../../src/constants';
 
-const testParameters = [
-  {
-    title: 'Cappuccino',
-    price: COFFEE_PRICES.cappuccino,
-    assert: (menuPage, priceStr) => menuPage.assertCoffeeCupCostHasValue('Cappuccino', priceStr),
-  },
-  {
-    title: 'Espresso',
-    price: COFFEE_PRICES.espresso,
-    assert: (menuPage, priceStr) => menuPage.assertCoffeeCupCostHasValue('Espresso', priceStr),
-  },
-];
+let testParameters = [];
 
-testParameters.forEach(({ price, title, assert }) => {
-  test(`Check ${title} cup has correct cost`, async ({
+for (const [key, value] of Object.entries(COFFEE_NAMES)) {
+  testParameters.push({ coffee: value, price: COFFEE_PRICES[key] });
+}
+
+testParameters.forEach(({ price, coffee }) => {
+  test(`Check ${coffee} cup has correct cost`, async ({
     menuPage,
   }) => {
   const priceStr = priceFormatStr(price);
 
   await menuPage.open();
-  await assert(menuPage, priceStr);
+  await menuPage.assertCoffeeCupCostHasValue(coffee, priceStr);
   });
 });
